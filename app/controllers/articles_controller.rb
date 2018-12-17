ffeb583214d1399598558b27526fd854e7f14a27
Damon Collins <damon.c.p.collins@gmail.com>
2018-12-17 03:12:04 -0500

    done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,7 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+
   end
 
   def create
@@ -20,4 +21,14 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+    render :"/articles/_form"
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
+  end
 end
